831e9a832aa0aa3c6f4ef5278be474b5bc71003c
Florian Pichler <fp@ylk.gd>
2025-08-07 14:58:05 +0200

    Up timeout for the smoke test
    CI is slower than my local machine

--- test/cli.test.js
+++ test/cli.test.js
@@ -3,42 +3,48 @@ import { execa } from "execa";
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 
 describe("CLI Tests", () => {
-  describe("Returns result for an Ember CLI app", () => {
-    const killStrays = async () => {
-      try {
-        await execa("pkill", ["-9", "-f", "ember"], { reject: false });
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-      } catch {}
-    };
+  describe(
+    "Returns result for an Ember CLI app",
+    () => {
+      const killStrays = async () => {
+        try {
+          await execa("pkill", ["-9", "-f", "ember"], { reject: false });
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        } catch {}
+      };
 
-    beforeEach(killStrays);
-    afterEach(killStrays);
+      beforeEach(killStrays);
+      afterEach(killStrays);
 
-    it("should return measurements", async () => {
-      const cmd = await execa(
-        "node",
-        [
-          "../bin/cli.js",
-          "--command",
-          "pnpm start",
-          "--wait-for",
-          "h1",
-          "--file",
-          "./app/router.js",
-        ],
-        {
-          cwd: resolve(__dirname, "..", "test-ember-app"),
-        },
-      );
+      it("should return measurements", async () => {
+        const cmd = await execa(
+          "node",
+          [
+            "../bin/cli.js",
+            "--command",
+            "pnpm start",
+            "--wait-for",
+            "h1",
+            "--file",
+            "./app/router.js",
+          ],
+          {
+            cwd: resolve(__dirname, "..", "test-ember-app"),
+          },
+        );
 
-      console.log(cmd.stdout);
-      console.error(cmd.stderr);
+        console.log(cmd.stdout);
+        console.error(cmd.stderr);
 
-      expect(cmd.stdout).toContain("Measurement completed successfully!");
-      expect(cmd.stdout).toContain("# Performance Results");
-      expect(cmd.stdout).toContain(
-        "| Dev Server Ready | First Paint | App Loaded | Reload after change |",
-      );
-    });
-  });
+        expect(cmd.stdout).toContain("Measurement completed successfully!");
+        expect(cmd.stdout).toContain("# Performance Results");
+        expect(cmd.stdout).toContain(
+          "| Dev Server Ready | First Paint | App Loaded | Reload after change |",
+        );
+      });
+    },
+    {
+      timeout: 120000,
+    },
+  );
 });
